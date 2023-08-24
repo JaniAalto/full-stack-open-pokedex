55562823bfb65b6b2fa1c96443d7e7d3e62aa10b
Jani Aalto <janiaalto@outlook.com>
2023-08-24 05:44:39 +0300

    Tenth commit

--- cypress/e2e/pokemon.cy.js
+++ cypress/e2e/pokemon.cy.js
@@ -1,7 +1,17 @@
-describe('Pokedex', function() {
-  it('front page can be opened', function() {
+describe('Pokedex', function () {
+  it('Front page can be opened', function () {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('Pokemon page can be navigated to', function () {
+    //cy.visit('http://localhost:5000/pokemon/venusaur')
+    // cannot directly visit page for some reason; returns 404
+
+    cy.visit('http://localhost:5000')
+    cy.get('.list-item:first').click()
+    cy.contains('overgrow')
+    cy.contains('chlorophyll')
   })
 })
